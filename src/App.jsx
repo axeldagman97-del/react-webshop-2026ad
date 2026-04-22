2b36fb74bcfbf08f4ce2e22039d02a522ba35c8c
Axel Dagman <axeldagman97@gmail.com>
2026-04-22 15:34:37 +0200

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './MyHeader.jsx';
 import SearchUi from './SearchUi.jsx';
+import ProductCard from './ProductCard.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <SearchUi />
+      <ProductCard />
     </>
   );
 }
